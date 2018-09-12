4c24b57b9a708b744d214313ba46fdbe501c2f92
PavelShvykyj <myserver1978@gmail.com>
2018-09-13 00:27:47 +0300

    доработка компонента выбора даты

--- src/app/HallBrowser/movie-selector/movie-selector.component.ts
+++ src/app/HallBrowser/movie-selector/movie-selector.component.ts
@@ -3,7 +3,7 @@ import * as _ from 'underscore';
 import {RequestRouterService}  from '../../back-end-router/request-router.service';
 import { ISessionData, IGetMovieResponseViewModel, IGetSessionResponseViewModel } from '../../iback-end';
 import { IdataObject } from '../idata-object';
-import * as $ from 'jquery'; 
+import 'jquery'; 
 import 'jquery-ui/ui/widgets/datepicker.js';
 
 
@@ -29,7 +29,9 @@ export class MovieSelectorComponent implements OnInit, AfterViewInit {
   currentSessions : Array<any> = [];
   sessionData : ISessionData;
   @Output() sessionDataChange = new EventEmitter();
-  isHidden : boolean = true;
+  pickerHidden : boolean = true;
+  elDatePicker;
+  elDateList ;  
 
   constructor(private apiServis : RequestRouterService) { }
   ngOnInit() {
@@ -53,8 +55,17 @@ export class MovieSelectorComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(){
     
     //($("#datepicker") as any).datepicker();
-     this.isHidden = true;
+     //this.pickerHidden = true;
+     this.elDatePicker = ($("#inputGroupPickDay") as any);
+     this.elDateList   = ($("#inputGroupSelectDay") as any);
      
+     this.elDatePicker.datepicker(
+       {onSelect : (value, el)=> this.OnSelectDatePicker(value, el),
+        minDate: 0,
+        dateFormat: "yy.mm.dd",
+        dayNamesMin: [ "Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб" ],
+        monthNames: [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ]
+     }) 
   }
 
 
@@ -114,21 +125,31 @@ export class MovieSelectorComponent implements OnInit, AfterViewInit {
     });
   }
 
-  dateshow(){
-    this.isHidden = false;
-    setTimeout(()=>{($("#datepicker") as any)
-                    .datepicker(
-                      {onSelect : (value, el) =>
-                        {
-                        ($("#datepicker") as any).datepicker("hide"); 
-                        this.isHidden = true; 
-                        console.log(value)
-                        },
-                      controlType: "select"
-                     }
-                     )
-                     .datepicker("show");},100);
-     
+  OnSelectDatePicker(value, el){
+    this.OnChangeDate(Date.parse(value));
+  }
+
+  DatePickerChange(){
+    this.pickerHidden = !this.pickerHidden;
+    this.OnChangeDate(Date.now());
+    if (!this.pickerHidden)
+    {
+      
+      setTimeout(()=>{
+        this.elDatePicker.datepicker("show");
+         
+        this.elDatePicker.val(new Date().toLocaleString("ru", {  year: 'numeric',
+        month: 'numeric',
+        day: 'numeric'}))
+        },100);
+    } 
+    else
+    {
+      
+      this.elDatePicker.datepicker("hide"); 
+      $('select option[value='+this.currentDays[0].id+']').prop('selected', true);
+      
+    }
   }
 
 }
